2b7231968d541310a2244609381b954f70339e0b
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-04-14 10:45:30 -0400

    partial work

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -4,7 +4,7 @@
 	import type { Game } from '$lib/types';
 	import { v4 as uuid } from 'uuid';
 
-	let timer: boolean = true;
+	let isTimer: boolean = true;
 	let difficulty: string = 'medium';
 	let size: number = 9;
 
@@ -13,8 +13,14 @@
 			id: uuid(),
 			currentBoard: [],
 			correctBoard: [],
+			originalBoard: [],
+			mistakes: [],
+			timer: {
+				state: 'stopped',
+				duration: 0
+			},
 			props: {
-				timer,
+				isTimer,
 				difficulty,
 				size
 			}
@@ -55,6 +61,7 @@
 		}
 
 		game.currentBoard = grid;
+		game.originalBoard = grid.map((row: number[]) => [...row]);
 
 		localStorage.setItem(game.id, JSON.stringify(game));
 
@@ -68,36 +75,36 @@
 		<p class="text-lg">A simple sudoku game</p>
 	</header>
 
-	<div class="flex flex-col items-center w-1/4">
-		<div class="flex flex-row items-center w-full justify-between">
+	<div class="flex w-1/4 flex-col items-center">
+		<div class="flex w-full flex-row items-center justify-between">
 			<p class="p-5 text-lg">Timer</p>
 			<div class="inline-flex rounded-md shadow-sm" role="group">
 				<button
-					on:click={() => (timer = true)}
+					on:click={() => (isTimer = true)}
 					type="button"
-					class:active={timer === true}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-gray-100 active:text-blue-700"
+					class:active={isTimer === true}
+					class="rounded-l-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:bg-gray-100 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					Yes
 				</button>
 				<button
-					on:click={() => (timer = false)}
+					on:click={() => (isTimer = false)}
 					type="button"
-					class:active={timer === false}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-grey-100 active:text-blue-700"
+					class:active={isTimer === false}
+					class="active:bg-grey-100 rounded-r-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					No
 				</button>
 			</div>
 		</div>
-		<div class="flex flex-row items-center  w-full justify-between">
+		<div class="flex w-full flex-row items-center justify-between">
 			<p class="p-5 text-lg">Difficulty</p>
 			<div class="inline-flex rounded-md shadow-sm" role="group">
 				<button
 					on:click={() => (difficulty = 'easy')}
 					type="button"
 					class:active={difficulty === 'easy'}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-gray-100 active:text-blue-700"
+					class="rounded-l-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:bg-gray-100 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					Easy
 				</button>
@@ -105,7 +112,7 @@
 					on:click={() => (difficulty = 'medium')}
 					type="button"
 					class:active={difficulty === 'medium'}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-grey-100 active:text-blue-700"
+					class="active:bg-grey-100 border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					Medium
 				</button>
@@ -113,20 +120,20 @@
 					on:click={() => (difficulty = 'hard')}
 					type="button"
 					class:active={difficulty === 'hard'}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-grey-100 active:text-blue-700"
+					class="active:bg-grey-100 rounded-r-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					Hard
 				</button>
 			</div>
 		</div>
-		<div class="flex flex-row items-center  w-full justify-between">
+		<div class="flex w-full flex-row items-center justify-between">
 			<p class="p-5 text-lg">Size</p>
 			<div class="inline-flex rounded-md shadow-sm" role="group">
 				<button
 					on:click={() => (size = 4)}
 					type="button"
 					class:active={size === 4}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-gray-100 active:text-blue-700"
+					class="rounded-l-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:bg-gray-100 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					4x4
 				</button>
@@ -134,7 +141,7 @@
 					on:click={() => (size = 9)}
 					type="button"
 					class:active={size === 9}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-grey-100 active:text-blue-700"
+					class="active:bg-grey-100 border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					9x9
 				</button>
@@ -142,7 +149,7 @@
 					on:click={() => (size = 16)}
 					type="button"
 					class:active={size === 16}
-					class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white active:bg-grey-100 active:text-blue-700"
+					class="active:bg-grey-100 rounded-r-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 				>
 					16x16
 				</button>
@@ -155,8 +162,8 @@
 			>Start</a
 		> -->
 		<button
-			on:click={() => createGame({ timer, difficulty, size })}
-			class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg rounded-r-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+			on:click={() => createGame({ isTimer, difficulty, size })}
+			class="rounded-l-lg rounded-r-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
 		>
 			Start
 		</button>
