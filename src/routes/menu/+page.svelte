ce87137428262563cdf333d7d498f17a5241d7fc
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-04-17 07:48:08 -0400

    disable alternative sizes

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -136,7 +136,8 @@
 					on:click={() => (size = 4)}
 					type="button"
 					class:active={size === 4}
-					class="rounded-l-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:bg-gray-100 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
+					disabled={true}
+					class="rounded-l-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
 				>
 					4x4
 				</button>
@@ -152,7 +153,8 @@
 					on:click={() => (size = 16)}
 					type="button"
 					class:active={size === 16}
-					class="active:bg-grey-100 rounded-r-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 active:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
+					disabled={true}
+					class="rounded-r-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
 				>
 					16x16
 				</button>
